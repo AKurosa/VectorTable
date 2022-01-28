d2d7a95dbec6ee79c319e78b1eff3d58b38ad069
AKurosa <98587872+AKurosa@users.noreply.github.com>
2022-01-29 08:04:02 +0900

    Change input from text to object

--- vectorTable.js
+++ vectorTable.js
@@ -3,7 +3,7 @@ const theXmlns = "http://www.w3.org/2000/svg";
 let elements = new Array();
 
 //Window Resize Event
-function resizeWindow(event){
+function _vtResizeWindow(event){
     elements.forEach(elem =>{
         let elemWidth = elem.getBoundingClientRect().width;
         let elemHeight = elem.getBoundingClientRect().height;
@@ -16,36 +16,49 @@ function resizeWindow(event){
     });
 }
 
-window.addEventListener('resize', resizeWindow);
+window.addEventListener('resize', _vtResizeWindow);
 
-//Append SVG Area to Elem
-function appendSvgArea(id)
+//check setting
+function _vtCheckSetting(setting)
 {
-    //Get element by id
-    let elem = document.getElementById(id);
+    //If No id key
+    if(!("id" in setting)){
+        throw "No 'id' in setting " + theProjectName;
+    }
 
-    //If can not get element by id
+    //If No element
+    let elem = document.getElementById(setting.id);
     if(elem==null)
     {
-        throw "Can not get element by id (" + arguments.callee.name + ") " + theProjectName;
+        throw "No 'element' id=" + setting.id + " " + theProjectName;
     }
+}
+
+//Append SVG Area to Elem
+function _vtAppendSvgArea(setting)
+{
+    //Get element by id
+    let elem = document.getElementById(setting.id);
 
     //Get element's width and height
     let elemWidth = elem.getBoundingClientRect().width;
     let elemHeight = elem.getBoundingClientRect().height;
     let viewBoxText = "0 0 " + elemWidth + " " + elemHeight;
     
+    //Append svg
     let svg = document.createElementNS(theXmlns,"svg");
     svg.setAttribute("width", elemWidth);
     svg.setAttribute("height", elemHeight);
     svg.setAttribute("viewBox", viewBoxText);
     elem.appendChild(svg);
 
+    //Push to Global element array
     elements.push(elem);
 }
 
 //add Vector Table to Elem
-function addVectorTable(id)
+function addVectorTable(setting)
 {
-    appendSvgArea(id)
+    _vtCheckSetting(setting)//check setting
+    _vtAppendSvgArea(setting)//Append SVG Area to Elem
 }
