e48fc08c0c6b08d8ee9e6243938b2f7d1df98241
AKurosa <98587872+AKurosa@users.noreply.github.com>
2022-01-29 17:13:08 +0900

    Set Outer Frame

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # VectorTable
 The html table using SVG and javascript
 
+How to use:
+* See "sample.html"
+
 Setting:  
 * id:  
   * element's id for Vector Table.  
@@ -8,5 +11,23 @@ Setting:
   * For set background color.  
   * Default value is "white".  
   * You can use svg fill color.  
-      * "black", "gray" , "#ccc", ...   
+      * "black", "gray" , "#ccc", ...  
+* row_dir_line:
+  * Whether to drow row direction line.
+  * true: drow, false: not.
+  * Default value is "false"
+* col_dir_line:
+  * Whether to drow column direction line.
+  * true: drow, false: not.
+  * Default value is "false"
+* outer_frame (Option):
+  * Whether to draw outer frame or not.
+  * true: draw, false: not.
+  * Default value is "false"
+* outer_frame_stroke_width (Option):
+  * Set outer frame's line width.
+  * Default value is 5
+* outer_frame_stroke (Option):
+  * Set outer frame's color.
+  * Default value is SVG's default (Maybe black).
 
--- vectorTable.js
+++ vectorTable.js
@@ -1,8 +1,14 @@
 const theProjectName = "[vectorTable]"
 const theXmlns = "http://www.w3.org/2000/svg";
-let elements = new Array();
-const class_backGround = "_vtBackGround"
-const key_backGroundColor = "background_color"
+let elements = new Array(); 
+
+const class_backGround = "_vtBackGround";
+const class_outerFrame = "_vtOuterFrame";
+
+const key_backGroundColor = "background_color";
+
+const at_rowLineWidth = "a_rlw";
+const at_colLineWidth = "a_clw";
 
 //Window Resize Event
 function _vtResizeWindow(event){
@@ -12,14 +18,36 @@ function _vtResizeWindow(event){
         let viewBoxText = "0 0 " + elemWidth + " " + elemHeight;
 
         let ef = elem.firstElementChild;
-        ef.setAttribute("width", elemWidth );
+        let old_width = Number(ef.getAttribute("width"));
+        let old_height = Number(ef.getAttribute("height"));
+        ef.setAttribute("width", elemWidth);
         ef.setAttribute("height", elemHeight);
         ef.setAttribute("viewBox", viewBoxText);
 
+        //Back Ground
         let backgrounds = ef.getElementsByClassName(class_backGround);
         Array.from(backgrounds).forEach(b =>{
             b.setAttribute("width", elemWidth);
             b.setAttribute("height", elemHeight);
+        });
+
+        //Outer Line
+        let outerFrame = ef.getElementsByClassName(class_outerFrame);
+        Array.from(outerFrame).forEach(o =>{
+            let children = o.children;
+            Array.from(children).forEach(c =>{
+                let old_x1 = Number(c.getAttribute("x1"));
+                c.setAttribute("x1", old_x1 * elemWidth / old_width);
+
+                let old_x2 = Number(c.getAttribute("x2"));
+                c.setAttribute("x2", old_x2 * elemWidth / old_width);
+
+                let old_y1 = Number(c.getAttribute("y1"));
+                c.setAttribute("y1", old_y1 * elemHeight / old_height);
+
+                let old_y2 = Number(c.getAttribute("y2"));
+                c.setAttribute("y2", old_y2 * elemHeight / old_height);
+            });
         });
     });
 }
@@ -60,6 +88,98 @@ function _vtGenBackGround(setting, w, h)
     return background
 }
 
+//Generate outer frame
+function _vtGenOuterFrame(setting, w, h)
+{
+    if(!("outer_frame" in setting)){
+        return false;//if no key
+    }else{
+        if(!setting.outer_frame){
+            return null//if value is false
+        }
+    }
+
+    let g = document.createElementNS(theXmlns,"g");
+    g.classList.add(class_outerFrame);
+
+    let stroke_width = 5*2;
+    if("outer_frame_stroke_width" in setting){
+        stroke_width = setting.outer_frame_stroke_width;
+    }
+
+    //Row Line
+    if("row_dir_line" in setting){
+        if(setting.row_dir_line){//if drow row line
+            //upper side line
+            let uLine = document.createElementNS(theXmlns,"line");
+            uLine.setAttribute("x1", 0);
+            uLine.setAttribute("x2", w);
+            uLine.setAttribute("y1", 0);
+            uLine.setAttribute("y2", 0);
+            uLine.setAttribute("stroke-width", stroke_width*2);
+            if("outer_frame_stroke" in setting){
+                uLine.setAttribute("stroke", setting.outer_frame_stroke);
+            }
+            g.appendChild(uLine);
+
+            //lower side line
+            let dLine = document.createElementNS(theXmlns, "line");
+            dLine.setAttribute("x1", 0);
+            dLine.setAttribute("x2", w);
+            dLine.setAttribute("y1", h);
+            dLine.setAttribute("y2", h);
+            dLine.setAttribute("stroke-width", stroke_width*2);
+            if("outer_frame_stroke" in setting){
+                dLine.setAttribute("stroke", setting.outer_frame_stroke);
+            }
+            g.appendChild(dLine);
+
+            g.setAttribute(at_rowLineWidth, stroke_width)
+        }else{//if not drow row line
+            g.setAttribute(at_rowLineWidth, 0);
+        }
+    }else{
+        g.setAttribute(at_rowLineWidth, 0);
+    }
+
+    //Col Line
+    if("col_dir_line" in setting){
+        if(setting.col_dir_line){
+            //Left side line
+            let lLine = document.createElementNS(theXmlns,"line");
+            lLine.setAttribute("x1", 0);
+            lLine.setAttribute("x2", 0);
+            lLine.setAttribute("y1", 0);
+            lLine.setAttribute("y2", h);
+            lLine.setAttribute("stroke-width", stroke_width*2);
+            if("outer_frame_stroke" in setting){
+                lLine.setAttribute("stroke", setting.outer_frame_stroke);
+            }
+            g.appendChild(lLine);
+
+            //Right side line
+            let rLine = document.createElementNS(theXmlns, "line");
+            rLine.setAttribute("x1", w);
+            rLine.setAttribute("x2", w);
+            rLine.setAttribute("y1", 0);
+            rLine.setAttribute("y2", h);
+            rLine.setAttribute("stroke-width", stroke_width*2);
+            if("outer_frame_stroke" in setting){
+                rLine.setAttribute("stroke", setting.outer_frame_stroke);
+            }
+            g.appendChild(rLine);
+
+            g.setAttribute(at_colLineWidth, stroke_width);
+        }else{
+            g.setAttribute(at_colLineWidth, 0);
+        }
+    }else{
+        g.setAttribute(at_colLineWidth, 0);
+    }
+
+    return g;
+}
+
 //Append SVG Area to Elem
 function _vtAppendSvgArea(setting)
 {
@@ -78,7 +198,15 @@ function _vtAppendSvgArea(setting)
     svg.setAttribute("viewBox", viewBoxText);
 
     //Append background
-    svg.appendChild(_vtGenBackGround(setting, elemWidth, elemHeight))
+    let bg = _vtGenBackGround(setting, elemWidth, elemHeight);
+    svg.appendChild(bg);
+
+    //Gen outer frame
+    let of = _vtGenOuterFrame(setting, elemWidth, elemHeight);
+    if(of != null){
+        //svg.insertBefore(of, bg);
+        svg.appendChild(of)
+    }
 
     elem.appendChild(svg);
 
